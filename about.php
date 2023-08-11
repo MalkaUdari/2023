ee6fb8766e52c6b85757a85c47aa14a2c5ae5f5e
MalkaUdari <134399860+MalkaUdari@users.noreply.github.com>
2023-08-12 06:40:00 +0900

    Add files via upload

---
+++ about.php
@@ -0,0 +1,113 @@
+<!--header-->
+<?php include 'INC_HEAD.php' ?>
+    <link rel="stylesheet" href="css/css.css">
+    <link rel="stylesheet" href="css/x2-css.css">
+    <link rel="stylesheet" href="css/x5-css.css">
+   
+    <title>about</title>
+    <style>
+        
+    </style>
+</head>
+<body>
+    <!--navi-->
+   <?php include 'INC_NAVI.php' ?>
+    <section id="xse1">
+        <div id="x5d5" class="x5d1">
+            <div class="x5d4">
+                <div class="w">
+                    <div class="x5d3">
+                        <h1 class="x5h1">about us</h1>
+                        <span class="x5s1">
+                            <a href="index.php" class="x5a1">home</a>
+                            <a href="about.php" class="x5a1">about us</a>
+                        </span>
+                    </div>
+                </div>
+             </div>
+        </div>
+        <div class="x2d17">
+            <div class="w">
+                <div id="x2d1">
+                    <div class="x2d2">
+                        <img src="img/pool.webp" alt="First_Pool" class="x2i1">
+                        <div class="x2d3">
+                            <img src="img/pool.webp" alt="Second_Pool" class="x2i2">
+                        </div>
+                    </div>
+                    <div class="x2d4">
+                        <hgroup>
+                        <span class="x2s1">about</span>
+                            <h2 class="x2h21">we provide best place to enjoy your time</h2>
+                            <p class="x2p1" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi dolorem qui, id provident voluptates laborum nobis. Aperiam quis voluptas qui.</p>
+                        </hgroup>
+                        <div class="x2d6">
+                            <p class="x2p2">Lorem ipsum dolor sit amet consectetur, atibus?</p>
+                            <p class="x2p2">Lorem ipsum dolor sit amet consectetur, atibus?</p>
+                            <p class="x2p2">Lorem ipsum dolor sit amet consectetur, atibus?</p>
+                        <a href="#" class="x2a7">discover more</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="x2d7">
+            <div class="x2d8">
+                <div class="w">
+                    <hgroup class="hg">
+                        <span class="x2s2">testmoiya</span>
+                            <h2 class="x2h22">what clines says</h2>
+                            <p class="p1" id="p3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi dolorem qui, id provident voluptates laborum nobis. Aperiam quis voluptas qui.</p>
+                    </hgroup>
+                 <div id="x2d10">
+                        <div class="x2d11">
+                            <span class="xs7">
+                                <img src="img/icon/comma.svg" alt="Comma" class="x2i10">
+                            </span>
+                            <div class="x2d12">
+                                <img src="img/icon/star5.svg" alt="stars" class="x2i11">
+                                <img src="img/icon/star5.svg" alt="stars" class="x2i11">
+                                <img src="img/icon/star5.svg" alt="stars" class="x2i11">
+                                <img src="img/icon/star5.svg" alt="stars" class="x2i11">
+                                <img src="img/icon/star5.svg" alt="stars" class="x2i11">
+                                <P class="x2P3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore officia repellat, obcaecati fugit repellendus nisi eius recusandae sed. Sapiente voluptatem dolorem reiciendis doloribus rerum ab, ratione omnis. Assumenda, deserunt dolorem.</P>
+                            </div>
+                            <div class="x2d13">
+                                <div class="x2d14">
+                                    <img src="img/pic-2.webp" alt="Lady_Pic" class="x2i12">
+                                </div>
+                                <div class="x2d15">
+                                    <h3 class="x2h23">sylvia  h green </h3>
+                                    <p class="x2P3">Lorem ipsum dolor sit amet.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="x2d11">
+                            <span class="xs7">
+                                <img src="img/icon/comma.svg" alt="Comma" class="x2i10">
+                            </span>
+                            <div class="x2d12">
+                                <img src="img/icon/star5.svg" alt="stars" class="x2i11">
+                                <img src="img/icon/star5.svg" alt="stars" class="x2i11">
+                                <img src="img/icon/star5.svg" alt="stars" class="x2i11">
+                                <img src="img/icon/star5.svg" alt="stars" class="x2i11">
+                                <img src="img/icon/star5.svg" alt="stars" class="x2i11">
+                                <P class="x2P3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore officia repellat, obcaecati fugit repellendus nisi eius recusandae sed. Sapiente voluptatem dolorem reiciendis doloribus rerum ab, ratione omnis. Assumenda, deserunt dolorem.</P>
+                            </div>
+                            <div class="x2d13">
+                                <div class="x2d14">
+                                    <img src="img/pic-3.webp" alt="Lady_Pic" class="x2i12">
+                                </div>
+                                <div class="x2d15">
+                                    <h2 class="x2h23">glodan n noven</h2>
+                                    <p class="x2P3">Lorem ipsum dolor sit amet.</p>
+                                </div>
+                            </div>
+                        </div>
+                 </div>
+                </div>
+            </div>
+      </div>
+    </section>
+    <!---footer-->
+    <?php include 'INC_FOOT.php' ?>
